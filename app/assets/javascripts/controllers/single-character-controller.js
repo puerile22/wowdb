ea83457c5ca33ddd321f19f875c2b9d2332014f8
Peng Gao <puerile22@gmail.com>
2015-01-26 18:52:46 -0800

    added items to single character views

--- app/assets/javascripts/controllers/single-character-controller.js
+++ app/assets/javascripts/controllers/single-character-controller.js
@@ -1,7 +1,18 @@
 app.controller("SingleCharController", ["$scope", "CharFactory", '$route', function($scope, CharFactory, $route) {
+  $scope.itemArray = ['head', 'neck', 'shoulder', 'back', 'chest', 'shirt', 'tabard', 'wrist', 'hands', 'waist', 'legs', 'feet', 'finger1', 'finger2', 'trinket1', 'trinket2', 'mainHand', 'offHand'];
   CharFactory.singleCharacter($route.current.params, {fields: 'items'}).success(function(data) {
     $scope.character = data;
+    $scope.items = $scope.itemArray;
   });
+  $scope.hasItem = function(item) {
+    if ($scope.character.items[item]) {
+      return true;
+    } else {
+      return false;
+    };
+  };
+
+  $scope.itemBaseUrl = 'http://us.media.blizzard.com/wow/icons/56';
   $scope.imgBaseUrl = 'http://us.battle.net/static-render/us';
   $scope.race = {
     '1': 'Human',
